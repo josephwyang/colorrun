f7f05e5c8cb36d084a45e3315bb08c3cce3d357e
joseph <josephwyang@gmail.com>
2021-07-29 14:47:52 -0400

    add more variety of obstacles and refactor them to have dynamic characterstics

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -4,45 +4,48 @@ import Obstacle from "./obstacle";
 export default class Game {
   constructor() {
     this.player = new Player();
+    this.active = true;
     
     this.movePlayer = this.movePlayer.bind(this);
     this.stopPlayer = this.stopPlayer.bind(this);
     this.reset = this.reset.bind(this);
     this.spaceReset = this.spaceReset.bind(this);
-    
+
     document.querySelector("#restart-btn").onclick = this.reset;
     this.init();
+    
+    view.onFrame = e => {
+      if (this.active) {
+        if(e.count % 300 === 0) this.obstacles.push(new Obstacle());
+        for(const i in this.obstacles) {
+          if (this.obstacles[i].group.children.some(child => this.player.piece.getIntersections(child).length)) { this.gameOver(); }
+          this.obstacles[i].move();
+          if (!window.paper.project.activeLayer.isChild(this.obstacles[i].group)) { delete this.obstacles[i] }
+        }
+      }
+    }
   }
 
   init() {
     this.obstacles = [];
-
-    view.onFrame = e => {
-      if(e.count % 300 === 0) this.obstacles.push(new Obstacle());
-      for(const i in this.obstacles) {
-        if (this.obstacles[i].group.children.some(child => this.player.piece.getIntersections(child).length)) { this.gameOver(); }
-        this.obstacles[i].move();
-        if (!window.paper.project.activeLayer.isChild(this.obstacles[i].group)) { delete this.obstacles[i] }
-      }
-    }
+    this.active = true;
     window.addEventListener("keydown", this.movePlayer);
     window.addEventListener("keyup", this.stopPlayer);
   }
-  
+
   setRestartModal(display) { document.querySelector(".modal").style.display = display; }
   movePlayer(e) { if (e.key.slice(0, 5) === "Arrow") this.player.move(e.key); }
   stopPlayer(e) { if (e.key.slice(0, 5) === "Arrow") this.player.stop(e.key); }
 
   gameOver() {
-    debugger
     window.removeEventListener("keydown", this.movePlayer);
     window.removeEventListener("keyup", this.stopPlayer);
 
-    // window.addEventListener("keydown", this.spaceReset)
+    window.addEventListener("keydown", this.spaceReset)
     this.setRestartModal("block");
-    view.onFrame = null;
     this.player.gameOver();
-    this.obstacles.forEach(obstacle => obstacle.stop());
+    this.obstacles.forEach(obstacle => obstacle.group.remove());
+    this.active = false;
   }
 
   spaceReset(e) {
@@ -51,7 +54,7 @@ export default class Game {
 
   reset(e) {
     e.preventDefault();
-    // window.removeEventListener("keydown", this.spaceReset)
+    window.removeEventListener("keydown", this.spaceReset)
     this.setRestartModal("none");
 
     this.player.reset();
--- src/scripts/obstacle.js
+++ src/scripts/obstacle.js
@@ -5,92 +5,91 @@ export default class Obstacle {
     this.height = view.bounds.height;
     this.speed = 6;
 
-    this[`draw${Math.floor(Math.random()*2)}`]();
+    this[`draw${Math.floor(Math.random()*4)}`]();
+    // this.draw2();
   }
   
   move() {
     if(this.group.position.x < -view.bounds.width) this.group.remove();
     else { this.group.position.x -= this.speed }
   }
-
-  stop() {
-    this.group.remove();
-    this.group.children.forEach(child => child.onFrame = undefined);
-  }
   
+  // arcs
   draw0() {
-    const currentX = dx => this.initialX - (dx * this.speed);
+    const numPaths = 4;
     const rotationSpeed = this.speed;
+    const currentX = dx => this.initialX - (dx * this.speed);
 
-    const path1 = new Path.Arc({
-      from: [this.initialX, 0],
-      through: [this.initialX, this.height/4],
-      to: [this.initialX + this.height/8, this.height/8],
-      strokeColor: this.strokeColor
-    });
-    path1.onFrame = e => { path1.rotate(rotationSpeed, new Point(currentX(e.count) - this.speed, this.height/8)) };
+    const group = [];
+    for(let i = 0; i < numPaths; i++) {
+      group.push(new Path.Arc({
+        from: [this.initialX, (this.height * i)/numPaths],
+        through: [this.initialX, (this.height * (i + 1))/numPaths],
+        to: [this.initialX + (this.height/(numPaths * 2)), (this.height * (2 * i + 1))/(numPaths * 2)],
+        strokeColor: this.strokeColor,
+      }));
+      group[i].rotate(90* i);
+      group[i].onFrame = e => group[i].rotate(rotationSpeed, new Point(currentX(e.count) - this.speed, (this.height * (2 * i + 1))/(numPaths * 2)))
+    };
 
-    const path2 = new Path.Arc({
-      from: [this.initialX + this.height/8, 3*this.height/8],
-      through: [this.initialX, this.height/4],
-      to: [this.initialX, this.height/2],
-      strokeColor: this.strokeColor
-    });
-    path2.onFrame = e => path2.rotate(rotationSpeed, new Point(currentX(e.count) - this.speed, 3*this.height/8));
-
-    const path3 = new Path.Arc({
-      from: [this.initialX, 3*this.height/4],
-      through: [this.initialX, this.height/2],
-      to: [this.initialX - this.height/8, 5*this.height/8],
-      strokeColor: this.strokeColor
-    });
-    path3.onFrame = e => path3.rotate(rotationSpeed, new Point(currentX(e.count) - this.speed, 5*this.height/8));
-
-    const path4 = new Path.Arc({
-      from: [this.initialX - this.height/8, 7*this.height/8],
-      through: [this.initialX, this.height],
-      to: [this.initialX, 3*this.height/4],
-      strokeColor: this.strokeColor
-    });
-    path4.onFrame = e => path4.rotate(rotationSpeed, new Point(currentX(e.count) - this.speed, 7*this.height/8));
-
-    this.group = new Group([path1, path2, path3, path4]);
+    this.group = new Group(group);
   }
 
+  // lines
   draw1() {
+    const numPaths = 3;
     const rotationSpeed = this.speed * 1.5;
 
-    const path1 = new Path.Line({
-      from: [this.initialX, 0],
-      to: [this.initialX, this.height/3],
-      strokeColor: this.strokeColor
-    });
-    path1.onFrame = () => path1.rotate(rotationSpeed);
+    const group = [];
+    for(let i = 0; i < numPaths; i++) {
+      group.push(new Path.Line({
+        from: [this.initialX, this.height * i / numPaths],
+        to: [this.initialX, this.height * (i + 1) / numPaths],
+        strokeColor: this.strokeColor
+      }));
+      group[i].rotate(90* i);
+      group[i].onFrame = () => group[i].rotate(rotationSpeed);
+    }
 
-    const path2 = new Path.Line({
-      from: [this.initialX, this.height/3],
-      to: [this.initialX, 2*this.height/3],
-      strokeColor: this.strokeColor
-    });
-    path2.onFrame = () => path2.rotate(-rotationSpeed);
-
-    const path3 = new Path.Line({
-      from: [this.initialX, 2*this.height/3],
-      to: [this.initialX, this.height],
-      strokeColor: this.strokeColor
-    })
-    path3.onFrame = () => path3.rotate(rotationSpeed);
-
-    this.group = new Group([path1, path2, path3]);
+    this.group = new Group(group);
   }
 
+  // windmill
   draw2() {
-    this.group = new Path.Star(new Point(400, 400), 5, 80, 150);
-    this.group.strokeColor = this.strokeColor;
-    this.group.splitAt(0);
-    this.group.removeSegments(0,2);
-    this.group.onFrame = e => {
-      this.group.rotate(1);
+    const numPaths = 8;
+    const bladeWidth = 75;
+    const rotationSpeed = this.speed/6;
+
+    const group = [];
+    for(let i = 0; i < numPaths; i++) {
+      const blade = new Path();
+      blade.add(new Point(this.initialX + this.height/2 - bladeWidth, 0));
+      blade.add(new Point(this.initialX + this.height/2 + bladeWidth, 0));
+      blade.add(new Point(this.initialX + this.height/2, this.height/2));
+      blade.closed = true;
+      blade.strokeColor = this.strokeColor;
+      blade.rotate((360/numPaths) * i, new Point(this.initialX + this.height/2, this.height/2));
+      group.push(blade)
     }
+
+    this.group = new Group(group);
+    this.group.onFrame = () => this.group.rotate(rotationSpeed);
+  }
+
+  // stars
+  draw3() {
+    const numPaths = 2;
+    const rotationSpeed = this.speed/2;
+    const currentX = dx => this.initialX - (dx * this.speed);
+
+    const group = [];
+    for(let i = 0; i < numPaths; i++) {
+      group.push(new Path.Star(new Point(this.initialX, (this.height * (2 * i + 1))/(numPaths * 2)), 5, this.height/(numPaths * 2)/2, this.height/(numPaths * 2)));
+      group[i].strokeColor = this.strokeColor;
+      group[i].rotate(36 * i);
+      group[i].onFrame = e => group[i].rotate(rotationSpeed, new Point(currentX(e.count), (this.height * (2 * i + 1))/(numPaths * 2)));
+    }
+
+    this.group = new Group(group);
   }
 }
